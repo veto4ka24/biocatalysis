8a9e22a6b9f0aa5f7ee9b658c39c982f041eb90c
veto4ka24 <zhbanova.es@phystech.edu>
2023-04-07 15:45:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pandas as pd
+from functools import reduce
+from typing import Union
+
+df1 = pd.read_csv('MZ78_sle_protein_peptide_precipetation.csv', sep=';')
+df2 = pd.read_csv('MZ81_sle_protein_peptide_superdex75.csv', sep=';')
+df1 = df1.filter(['Protein Accession', 'Peptide']).drop_duplicates()
+df2 = df2.filter(['Protein Accession', 'Peptide']).drop_duplicates()
+df2.head()
+
+result_2 = pd.merge(df1, df2, on = 'Protein Accession', how='inner')
 
 proteins_81 = pd.read_csv('MZ81_sle_protein_peptide_superdex75.csv', header = 0, names = ['protein', 'peptide'], sep = ';', usecols = [2, 3])
 df_10 = pd.DataFrame(proteins_81)
+prot_81 = pd.read_csv('MZ81_sle_protein_peptide_superdex75.csv', header = 0, names = ['protein'])
+df_11 = pd.DataFrame(prot_81)
 proteins_78 = pd.read_csv('MZ78_sle_protein_peptide_precipetation.csv', header = 0, names = ['protein', 'peptide'], sep = ';', usecols = [2, 3])
 df_20 = pd.DataFrame(proteins_78)
-result_1 = pd.merge(df_10, df_20, how = 'inner', on = ['protein', 'peptide'])
-print(result_1.to_string())
+prot_78 = pd.read_csv('MZ78_sle_protein_peptide_precipetation.csv', header = 0, names = ['protein'])
+df_21 = pd.DataFrame(prot_78)
+#result_1 = pd.merge(df_10, df_20, how = 'inner', on = ['protein', 'pepide'])
+print(result_2)
 
