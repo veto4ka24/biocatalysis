c70d27765bc2ddefc135b47fabce7bcbfd2b8461
veto4ka24 <zhbanova.es@phystech.edu>
2023-07-20 18:09:48 +0300

    нашлись НЕздоровые пептиды

--- cores_not_in_healthy.py
+++ cores_not_in_healthy.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+cores_78 = pd.read_csv('cores_78.csv', sep = ';')
+cores_78 = cores_78.filter(['Peptide', 'Core']).drop_duplicates()
+cores_81 = pd.read_csv('cores_81.csv', sep = ';')
+cores_81 = cores_81.filter(['Peptide', 'Core']).drop_duplicates()
 cores_78_1 = pd.read_csv('cores_78(precipetation)_in_phage_display_r1.csv', sep = ';')
 cores_78_1 = cores_78_1.filter(['Core']).drop_duplicates()
 cores_78_2 = pd.read_csv('cores_78(precipetation)_in_phage_display_r2.csv', sep = ';')
@@ -38,3 +42,29 @@ res_81_1_except_76 = cores_81_1.merge(cores_76, how = 'left', on = 'Core', indic
 res_81_2_except_76 = cores_81_2.merge(cores_76, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
 #res_81_2_except_76.to_csv("cores_81&r2_except_76.csv", index=False)
 
+#ищем пептиды, к которым относятся эти коры
+nothealthy_peps_78_1_no76 = res_78_1_except_76.merge(cores_78, how = 'inner', on = 'Core')
+#nothealthy_peps_78_1_no76.to_csv('peps_precipetation&PD1_not_in_76.csv', index = False)
+
+nothealthy_peps_78_2_no76 = res_78_2_except_76.merge(cores_78, how = 'inner', on = 'Core')
+#nothealthy_peps_78_2_no76.to_csv('peps_precipetation&PD2_not_in_76.csv', index = False)
+
+nothealthy_peps_78_1_no79 = res_78_1_except_79.merge(cores_78, how = 'inner', on = 'Core')
+#nothealthy_peps_78_1_no79.to_csv('peps_precipetation&PD1_not_in_79.csv', index = False)
+
+nothealthy_peps_78_2_no79 = res_78_2_except_79.merge(cores_78, how = 'inner', on = 'Core')
+#nothealthy_peps_78_2_no79.to_csv('peps_precipetation&PD2_not_in_79.csv', index = False)
+
+nothealthy_peps_81_1_no76 = res_81_1_except_76.merge(cores_81, how = 'inner', on = 'Core')
+#nothealthy_peps_81_1_no76.to_csv('peps_gelfiltration&PD1_not_in_76.csv', index = False)
+
+nothealthy_peps_81_2_no76 = res_81_2_except_76.merge(cores_81, how = 'inner', on = 'Core')
+#nothealthy_peps_81_2_no76.to_csv('peps_gelfiltration&PD2_not_in_76.csv', index = False)
+
+nothealthy_peps_81_1_no79 = res_81_1_except_79.merge(cores_81, how = 'inner', on = 'Core')
+#nothealthy_peps_81_1_no79.to_csv('peps_gelfiltration&PD1_not_in_79.csv', index = False)
+
+nothealthy_peps_81_2_no79 = res_81_2_except_79.merge(cores_81, how = 'inner', on = 'Core')
+#nothealthy_peps_81_2_no79.to_csv('peps_gelfiltration&PD2_not_in_79.csv', index = False)
+
+#ищем супер-пептиды (которые есть в нескольких файлах)
