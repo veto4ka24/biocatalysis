ce40ae80df1ff152bac26353a1d092849e05687e
veto4ka24 <zhbanova.es@phystech.edu>
2023-07-20 16:58:36 +0300

    cores_not_in_healthy
    нашли коры, которые есть только в масс-спектрах больных

--- cores_not_in_healthy.py
+++ cores_not_in_healthy.py
@@ -16,16 +16,25 @@ cores_79 = cores_79.filter(['Core']).drop_duplicates()
 
 #ищем коры, которые есть в ФД 1 и 2 раундов и в ИП больных, но нет в ГФ здоровых
 res_78_1_except_76 = cores_78_1.merge(cores_76, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_78_1_except_76.to_csv("cores_78&r1_except_76.csv", index=False)
 res_78_2_except_76 = cores_78_2.merge(cores_76, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_78_2_except_76.to_csv("cores_78&r2_except_76.csv", index=False)
 
 #ищем коры, которые есть в ФД 1 и 2 раундов и в ИП больных, но нет в ИП здоровых
 res_78_1_except_79 = cores_78_1.merge(cores_79, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_78_1_except_79.to_csv("cores_78&r1_except_79.csv", index=False)
 res_78_2_except_79 = cores_78_2.merge(cores_79, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_78_1_except_79.to_csv("cores_78&r2_except_79.csv", index=False)
 
 #ищем коры, которые есть в ФД 1 и 2 раундов и в ГФ больных, но нет в ИП здоровых
 res_81_1_except_79 = cores_81_1.merge(cores_79, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_81_1_except_79.to_csv("cores_81&r1_except_79.csv", index=False)
 res_81_2_except_79 = cores_81_2.merge(cores_79, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_81_2_except_79.to_csv("cores_81&r2_except_79.csv", index=False)
 
 #ищем коры, которые есть в ФД 1 и 2 раундов и в ГФ больных, но нет в ГФ здоровых
 res_81_1_except_76 = cores_81_1.merge(cores_76, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_81_1_except_76.to_csv("cores_81&r1_except_76.csv", index=False)
 res_81_2_except_76 = cores_81_2.merge(cores_76, how = 'left', on = 'Core', indicator = True).query("_merge == 'left_only'")
+#res_81_2_except_76.to_csv("cores_81&r2_except_76.csv", index=False)
+
